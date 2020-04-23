da633779a2dc6ac49bd9ba7025e02b527f6c0fba
phil <phil@phil.com>
2020-04-24 01:28:32 +0200

    Added a node to generate a point cloud out of the simulated tf and laser data from the new plugins. Added a launch file specificly for the hdl 32 velodyne

---
+++ ros/src/phros_gazebo_plugins/src/SensorVelodyne.cpp
@@ -0,0 +1,43 @@
+#include "SensorVelodyne.h"
+#include <laser_geometry/laser_geometry.h>
+#include <pcl_ros/point_cloud.h>
+#include <string>
+
+SensorVelodyne::SensorVelodyne()
+    : _listener(std::make_unique<tf::TransformListener>())
+    , _cloud(std::make_unique<pcl::PointCloud<pcl::PointXYZ> >())
+{
+  ros::NodeHandle prvNh("~");
+  std::string     topicScan;
+  std::string     topicCloud;
+  prvNh.param<std::string>("topic_scan", topicScan, "scan");
+  prvNh.param<std::string>("topic_cloud", topicCloud, "velodyne_points");
+  prvNh.param<std::string>("tf_base_frame", _tfBaseFrame, "base_link");
+  _subsScan = _nh.subscribe(topicScan, 1, &SensorVelodyne::callBackScan, this);
+  _pubCloud = _nh.advertise<pcl::PointCloud<pcl::PointXYZ> >(topicCloud, 1);
+  _cloud->header.frame_id = _tfBaseFrame;
+}
+
+SensorVelodyne::~SensorVelodyne(){};
+
+void SensorVelodyne::callBackScan(const sensor_msgs::LaserScan& scan)
+{
+  laser_geometry::LaserProjection projector;
+  if(!_listener->waitForTransform(scan.header.frame_id, _tfBaseFrame, scan.header.stamp + ros::Duration().fromSec(scan.ranges.size() * scan.time_increment),
+                                  ros::Duration(1.0)))
+  {
+    ROS_ERROR_STREAM(__PRETTY_FUNCTION__ << "timeout looking up transform from " << scan.header.frame_id << " to " << _tfBaseFrame);
+    return;
+  }
+
+  sensor_msgs::PointCloud cloud;
+  projector.transformLaserScanToPointCloud(_tfBaseFrame, scan, cloud, *_listener);
+  for(auto& iter : cloud.points)
+  {
+    pcl::PointXYZ pointPCL(iter.x, iter.y, iter.z);
+    _cloud->push_back(pointPCL);
+  }
+  _cloud->header.stamp = pcl_conversions::toPCL(ros::Time::now());
+  _cloud->header.seq++;
+  _pubCloud.publish(*_cloud);
+}
