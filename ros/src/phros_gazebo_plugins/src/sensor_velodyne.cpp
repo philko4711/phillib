dcb2e605bf97d83654aa69258c9e670cf242024d
Philipp Koch <philipp.koch@th-nuernberg.de>
2020-07-19 14:24:40 +0200

    Remove old obsolete velodyne plugins.

--- ros/src/phros_gazebo_plugins/src/sensor_velodyne.cpp
+++
@@ -1,9 +0,0 @@
-#include "SensorVelodyne.h"
-#include <ros/ros.h>
-int main(int argc, char** argv)
-{
-  ros::init(argc, argv, "sensor_velodyne");
-  SensorVelodyne sensorVelodyne;
-  sensorVelodyne.start();
-  return 0;
-}
