fe4030bd702e48d2a2402479fb354408ed9e185e
phil <philipp.koch@th-nuernberg.de>
2021-04-20 01:20:36 +0200

    Changed the pointer and new call with a unique_ptr

--- ros/src/phros_utils/src/cloud_transformer_pcl.cpp
+++ ros/src/phros_utils/src/cloud_transformer_pcl.cpp
@@ -18,13 +18,13 @@ void callBackCloud(const pcl::PointCloud<pcl::PointXYZ>& cloud);
 bool transFormPointCloud(pcl::PointCloud<pcl::PointXYZ>& in, tf::StampedTransform& tf);
 
 static ros::Publisher _pubTfCloud;
-static tf::TransformListener* _tfListener;
+static std::unique_ptr<tf::TransformListener> _tfListener;
 static std::string _tfChildFrame;
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "cloud_transformer");
-  _tfListener = new tf::TransformListener;
+  _tfListener = std::make_unique<tf::TransformListener>();
 
   ros::NodeHandle nh;
   ros::NodeHandle prvNh("~");
@@ -37,7 +37,6 @@ int main(int argc, char** argv)
   ros::Subscriber cloudSubs = nh.subscribe(inputTopic, 1, callBackCloud);
   _pubTfCloud = nh.advertise<pcl::PointCloud<pcl::PointXYZ> >(outputTopic, 1);
   ros::spin();
-  delete _tfListener;
 }
 
 void callBackCloud(const pcl::PointCloud<pcl::PointXYZ>& cloud)
