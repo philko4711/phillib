eb0c5bd90c25c1a7fc67ba75bbac3b2d1adf8ccf
phil <philipp.koch@th-nuernberg.de>
2021-05-21 22:55:30 +0200

    Added a new program to display laser rgb fusion.

---
+++ src/qt/ImageMagic.cpp
@@ -0,0 +1,13 @@
+#include "ImageMagic.h"
+
+namespace phillib
+{
+namespace qt
+{
+ImageMagic::ImageMagic()
+  {
+  }
+
+
+}
+}
---
+++ src/qt/ImageMagic.h
@@ -0,0 +1,18 @@
+#ifndef PHILLIB_QT_IMAGEMAGIC_H_
+#define PHILLIB_QT_IMAGEMAGIC_H_
+
+#include <QtCore/QObject>
+
+namespace phillib
+{
+namespace qt
+{
+  class ImageMagic : public QObject
+  {
+    ImageMagic();
+    virtual ~ImageMagic();
+};
+}
+}
+#endif
+
